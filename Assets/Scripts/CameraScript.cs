063cc9fba0cefcb4bd2364bf5c002ac26cda19af
Shreyansh <erixerns@gmail.com>
2020-01-11 18:41:43 +0530

    Added basic camera control

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+	public Transform target;
+	public float speed = 2f;
+	public float distance = 0.0f;
+	public float height = 2.0f;
+	public float heightDamping = 2.0f;
+
+	public float lookAtHeight = 0.0f;
+
+	public Rigidbody parentRigidbody;
+
+	public float rotationSnapTime = 2F;
+
+	public float distanceSnapTime = 0.1f;
+	public float distanceMultiplier = 0.1f;
+
+	private Vector3 lookAtVector;
+
+	private float usedDistance;
+
+	float wantedRotationAngle;
+	float wantedHeight;
+
+	float currentRotationAngle;
+	float currentHeight;
+
+	Quaternion currentRotation;
+	Vector3 wantedPosition;
+	private float yVelocity = 0.0F;
+	private float zVelocity = 0.0F;
+	void Start()
+	{
+		lookAtVector = new Vector3(0, lookAtHeight, 0);
+	}
+
+	void LateUpdate()
+	{
+		float interpolation = speed * Time.deltaTime;
+
+		wantedHeight = target.position.y + height;
+		currentHeight = transform.position.y;
+
+		wantedRotationAngle = target.eulerAngles.y;
+		currentRotationAngle = transform.eulerAngles.y;
+
+		// currentRotationAngle = Mathf.SmoothDampAngle(currentRotationAngle, wantedRotationAngle, ref yVelocity, rotationSnapTime);
+		currentRotationAngle = Mathf.Lerp(currentRotationAngle, wantedRotationAngle, rotationSnapTime);
+		currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+
+		wantedPosition = target.position;
+		wantedPosition.y = currentHeight;
+
+		usedDistance = Mathf.SmoothDampAngle(usedDistance, distance + (parentRigidbody.velocity.magnitude * distanceMultiplier), ref zVelocity, distanceSnapTime);
+
+		wantedPosition += Quaternion.Euler(0, currentRotationAngle, 0) * new Vector3(0, 0, -usedDistance);
+
+		// wantedPosition.x = Mathf.Lerp(transform.position.x, wantedPosition.x, interpolation);
+		// wantedPosition.y = Mathf.Lerp(transform.position.y, wantedPosition.y, interpolation);
+		// wantedPosition.z = Mathf.Lerp(transform.position.z, wantedPosition.z, interpolation);
+		wantedPosition = Vector3.Lerp(transform.position, wantedPosition, interpolation);
+		transform.position = wantedPosition;
+		transform.LookAt(target.position + lookAtVector);
+	}
+
+
+}
---
+++ Assets/Scripts/PlayerObjectScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerObjectScript : MonoBehaviour
+{
+	// Start is called before the first frame update
+	private GameObject rigidBody;
+	public float speed = 10f;
+	void Start()
+	{
+		rigidBody = GetComponent<GameObject>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.Translate(Vector3.forward * Time.deltaTime*speed);
+		}
+		else if (Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.Translate(Vector3.back * Time.deltaTime*speed);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Rotate(Vector3.up * -50*Time.deltaTime, Space.World);
+			// transform.Translate(Vector3.left * Time.deltaTime*speed);
+			// transform.RotateAround(this.transform.position, Vector3.up, 20 * Time.deltaTime);
+		}
+		else if (Input.GetKey(KeyCode.RightArrow))
+		{
+			// transform.Rotate(0, 10*Time.deltaTime, 0, Space.Self);
+			// transform.Translate(Vector3.right * Time.deltaTime*speed);
+			transform.Rotate(Vector3.up * 50*Time.deltaTime, Space.World);
+		}
+	}
+}
